bd2fe75a9f30b85985b5ccf872fda8e99edb23b7
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-01-25 20:47:27 +0900

    Javascriptの設定を修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("channels")
 require('jquery')
 require("cocoon")
 require("../test")
-require("../number")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
